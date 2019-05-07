67c95fc269006ec03d73f51d65f955bf713898ab
Petrov Andrew <andrew.petrov@emc.com>
2019-05-07 20:25:17 +0300

    Initial commit

---
+++ solver.py
@@ -0,0 +1,29 @@
+class ForwardSolver:
+    def __init__(self, f, n):
+        if not callable(f):
+            raise ValueError("f should be callable")
+        self._f = f
+        self._n = n
+        self._p = dict()
+        self._p[0] = {i: self._f(i) for i in range(1, self._n)}
+
+    def count(self):
+        for k in range(1, self._n):
+            j = 2 ** k
+            while j <= self._n - 2 ** k:
+                phi = self._p[k - 1][j - 2 ** (k - 1)] + self._p[k - 1][j + 2 ** (k - 1)]
+                self._p[k][j] = self._p[k - 1][j]
+                for l in range(1, 2 ** (k - 1)):
+                    self._p[k][j] += self._count_v(C, a, phi)
+                self._p[k][j] *= 0.5
+                j *= 2
+
+    def get_p(self):
+        return self._p
+
+    def _count_v(self, C, a, phi):
+        pass
+
+
+if __name__ == '__main__':
+    ForwardSolver(lambda x: x, 10)
